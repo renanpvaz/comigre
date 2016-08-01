36578c52383e0bcaf0d0224246dd6f20f385fa7a
Renan Vaz <renanp.vaz@outlook.com>
2016-08-01 11:10:46 -0300

    Adicionar gerador de componentes

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const gulp = require('gulp');
+const path = require('path');
+const rename = require('gulp-rename');
+const template = require('gulp-template');
+const yargs = require('yargs');
+const gutil = require('gulp-util');
+const del = require('del');
+
+const root = 'imports/components';
+
+let resolveToComponents = (glob = '') => {
+  return path.join(root, glob); // app/components/{glob}
+};
+
+// map of all paths
+let paths = {
+  js: resolveToComponents('**/*!(.test.js).js'), // exclude spec files
+  output: root,
+  blankTemplates: path.join(__dirname, '.generator', 'component/**/*.**'),
+  dest: path.join(__dirname, 'dist')
+};
+
+gulp.task('component', () => {
+  const cap = (val) => val.charAt(0).toUpperCase() + val.slice(1);
+  const name = yargs.argv.name;
+  const parentPath = yargs.argv.parent || '';
+  const destPath = path.join(resolveToComponents(), parentPath, name);
+
+  return gulp.src(paths.blankTemplates)
+    .pipe(template({
+      name: name,
+      upCaseName: cap(name)
+    }))
+    .pipe(rename((path) => {
+      path.basename = path.basename.replace('temp', name);
+    }))
+    .pipe(gulp.dest(destPath));
+});
+
+gulp.task('clean', (cb) => {
+  del([paths.dest]).then(function (paths) {
+    gutil.log("[clean]", paths);
+    cb();
+  })
+});
