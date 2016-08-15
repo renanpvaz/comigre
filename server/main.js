aa075986bfb8b59c4097175fec2e39b7abde1a9d
Renan Pires Vaz <renan.vaz@cwinet.local>
2016-08-15 14:59:47 -0300

    remover validação de arquivo de configurações

--- server/main.js
+++ server/main.js
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  if (!Object.keys(Meteor.settings.public).length) {
-    throw new Meteor.Error('settings', 'No settings.json file in the root of the project');
-  }
+  // if (!Object.keys(Meteor.settings.public).length) {
+  //   throw new Meteor.Error('settings', 'No settings.json file in the root of the project');
+  // }
 });
