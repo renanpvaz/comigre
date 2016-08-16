9b4ef49d2bd2bf02a0f565ab90972f7173b63c8a
Renan Pires Vaz <renan.vaz@cwinet.local>
2016-08-16 19:35:57 -0300

    adicionar posicionamento inicial do mapa quando usuário não for rastreado

--- imports/components/map/map.ctrl.js
+++ imports/components/map/map.ctrl.js
@@ -6,7 +6,16 @@ class mapCtrl {
 
     $reactive(this).attach($scope);
 
+    this.map = {
+      center: {
+        latitude: -10.1689,
+        longitude: -48.3317
+      },
+      zoom: 5
+    };
+
     this.places = [];
+    this.control = {};
 
     this.helpers({
       institutions() {
@@ -23,24 +32,27 @@ class mapCtrl {
           },
           zoom: 15
         };
+
+        uiGmapIsReady.promise().then(() => {
+          Meteor.call('getPlaces', this.map.center, 100,
+          (error, result) => {
+
+            if (!error) {
+              this.places = result.map((place, i) => ({
+                id: i,
+                location: {
+                  latitude: place.geometry.location.lat,
+                  longitude: place.geometry.location.lng
+                }
+              }));
+            }
+            if (this.places.length && this.control) {
+              this.control.newModels(this.places);
+            }
+          });
+        });
       });
     }
-
-    uiGmapIsReady.promise().then(() => {
-      Meteor.call('getPlaces', this.map.center, 100,
-      (error, result) => {
-
-        if (!error) {
-          this.places = result.map((place, i) => ({
-            id: i,
-            location: {
-              latitude: place.geometry.location.lat,
-              longitude: place.geometry.location.lng
-            }
-          }));
-        }
-      });
-    });
   }
 }
 
