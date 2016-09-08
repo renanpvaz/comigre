8d0b596fcc64c269f248e5663dc2fc30ffb9c7e3
renanpvaz <renanp.vaz@outlook.com>
2016-09-08 16:08:10 -0300

    teste gh pages removendo plataforma android

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -2,7 +2,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update gh-pages\n"
 
   #copy data we're interested in to other place
-  cp -R coverage $HOME/build.tgz
+  cp -R coverage $HOME/build
 
   #go to home and setup git
   cd $HOME
@@ -14,7 +14,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   #go into diractory and copy data we're interested in to that directory
   cd gh-pages
-  cp -Rf $HOME/build.tgz.
+  cp -Rf $HOME/build/* .
 
   #add, commit and push files
   git add -f .
