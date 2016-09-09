7428b545af39b2247755c5ac9fa3e532d5f1f2a2
renanpvaz <renanp.vaz@outlook.com>
2016-09-08 21:05:26 -0300

    adicionar opção no-cors

--- public/worker.js
+++ public/worker.js
@@ -8,10 +8,13 @@ if ('serviceWorker' in navigator) {
 
 const CACHE_NAME = 'comigre-cache';
 const urlsToCache = [
-  location.hostname === 'localhost' ? '/packages/modules.js?hash=*' : '/*.js?meteor_js_resource=true',
   'https://a.tile.openstreetmap.org/*/*/*.png',
   'https://b.tile.openstreetmap.org/*/*/*.png',
-  'https://c.tile.openstreetmap.org/*/*/*.png',
+  'https://c.tile.openstreetmap.org/*/*/*.png'
+];
+
+const urlsToPrefetch = [
+  '/packages/modules.js?hash=*',
   'https://cdnjs.cloudflare.com/ajax/libs/angular-material/1.1.0/angular-material.min.css',
   'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
@@ -21,7 +24,13 @@ const urlsToCache = [
 this.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        cache.addAll(urlsToPrefetch.map(url => {
+          return new Request(url, { mode: 'no-cors' });
+        })).then(() => {
+          console.log('All resources have been fetched and cached.');
+        });
+      })
   );
 });
 
