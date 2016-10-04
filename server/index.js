e2f75378ad84939e76f4d1f648f115028eb2f236
alanastank <alanass_12@hotmail.com>
2016-10-04 09:47:22 -0300

    adicionar collection guides

--- server/index.js
+++ server/index.js
@@ -2,3 +2,4 @@ import '../imports/api/places/collection';
 import '../imports/api/places/methods';
 import '../imports/api/places/publish';
 import '../imports/api/events/collection';
+import '../imports/api/guides/collection';
