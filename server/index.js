d9aea2c593e10bb170b227614ec88f0e55faeefc
alanastank <alanass_12@hotmail.com>
2016-10-05 16:54:02 -0300

    remover imports desnecessários

--- server/index.js
+++ server/index.js
@@ -1,7 +1,3 @@
 import '../imports/api/places/index';
-import '../imports/api/places/methods';
-import '../imports/api/places/publish';
 import '../imports/api/events/index';
-import '../imports/api/events/publish';
 import '../imports/api/guides/index';
-import '../imports/api/guides/publish';
