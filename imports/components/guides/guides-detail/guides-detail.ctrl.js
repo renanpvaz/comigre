3cdf86dfa1b21b784fa4d03a62d87fa59864b604
alanastank <alanass_12@hotmail.com>
2016-10-18 10:56:01 -0200

    adicionar redirecionamento para os detalhes de cada guia

--- imports/components/guides/guides-detail/guides-detail.ctrl.js
+++ imports/components/guides/guides-detail/guides-detail.ctrl.js
@@ -1,9 +1,26 @@
 'use strict';
 
-class GuidesDetailCtrl {
-  constructor() {
+import { Guides } from '../../../api/guides/collection.js';
+import Injectable from '../../../common/injectable';
+
+class GuidesDetailCtrl extends Injectable {
+  constructor($scope, $reactive, $stateParams) {
     'ngInject';
+
+    super(...arguments);
   }
+
+  $onInit() {
+    this.$reactive(this).attach(this.$scope);
+
+  this.helpers({
+    guide() {
+      return Guides.findOne({
+        _id: this.$stateParams.id
+      });
+    }
+  });
 }
 
+}
 export default GuidesDetailCtrl;
--- imports/components/guides/guides-detail/index.js
+++ imports/components/guides/guides-detail/index.js
@@ -1,12 +1,23 @@
 'use strict';
 
 import angular from 'angular';
+import uiRouter from 'angular-ui-router';
 
 import guidesDetailComponent from './guides-detail.component';
 
 const guidesDetail = angular
   .module('guidesDetail', [])
   .component('guidesDetail', guidesDetailComponent)
+  .config($stateProvider => {
+    'ngInject';
+
+    $stateProvider
+      .state('guides.detail', {
+        url: '^/guis/{id}',
+        parent: 'app',
+        component: 'guidesDetail'
+      });
+  })
   .name;
 
 export default guidesDetail;
