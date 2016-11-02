4fadb5bee3c3c97fc78c850cfe53f5663ed5b0c8
alanastank <alanass_12@hotmail.com>
2016-11-02 19:39:18 -0200

    atualizar meteor

--- imports/components/guides/guides.ctrl.js
+++ imports/components/guides/guides.ctrl.js
@@ -1,7 +1,18 @@
+import assign from 'angular-assign';
+
+import { Guides } from '../../api/guides/collection';
+
 class GuidesCtrl {
-  constructor() {
+  constructor($scope, $reactive) {
     'ngInject';
 
+    assign(arguments).to(this);
+  }
+
+  $onInit() {
+    this.$reactive(this).attach(this.$scope);
+
+    this.subscribe('allGuides');
   }
 }
 
