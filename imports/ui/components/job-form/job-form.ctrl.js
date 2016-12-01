903b5e9fa778e312759823d1fb30ac6512a46981
renanpvaz <renanp.vaz@outlook.com>
2016-12-01 19:44:42 -0200

    refatorar formulário de emprego

--- imports/ui/components/job-form/job-form.ctrl.js
+++ imports/ui/components/job-form/job-form.ctrl.js
@@ -2,6 +2,18 @@ class JobFormCtrl {
   constructor() {
     'ngInject';
   }
+
+  $onInit() {
+    this.job = {};
+  }
+
+  isContractTypeSelected(type) {
+    return this.job.contractType === type ? 'selected-cookie' : '';
+  }
+
+  setContractType(type) {
+    this.job.contractType = type;
+  }
 }
 
 export default JobFormCtrl;
