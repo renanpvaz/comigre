9a273b079789b13dca31c77f927705ccafb0dccd
renanpvaz <renanp.vaz@outlook.com>
2016-12-01 15:04:30 -0200

    adicionar publicação de usuários

---
+++ imports/api/users/index.js
@@ -0,0 +1,11 @@
+import { Meteor } from 'meteor/meteor';
+
+if (Meteor.isServer) {
+  Meteor.publish('users', function() {
+    return Meteor.users.find({}, {
+      fields: {
+        emails: 1
+      }
+    });
+  });
+}
